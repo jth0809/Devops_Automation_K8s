489162d31b2e936e76007acb708a1fe7bfb0f3d8
jth0809 <jth0809@naver.com>
2024-02-15 17:41:19 +0900

    update file

--- install.sh
+++ install.sh
@@ -13,17 +13,17 @@ sudo apt-get -y install helm
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
 
 echo "install cri-o podman kubelet kubeadm kubectl"
-curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" |
-    tee /etc/apt/sources.list.d/kubernetes.list
+    sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/Release.key |
+sudo curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/$PROJECT_PATH/deb/ /" |
-    tee /etc/apt/sources.list.d/cri-o.list
+    sudo tee /etc/apt/sources.list.d/cri-o.list
 
 sudo apt-get update
 sudo apt-get install -y cri-o podman kubelet kubeadm kubectl 
