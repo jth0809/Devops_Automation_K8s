b04dea0c1398fc09401cda32e19482cd2d98b0d7
jth0809 <jth0809@naver.com>
2024-02-15 16:21:43 +0900

    upload file

--- install.sh
+++ install.sh
@@ -13,8 +13,9 @@ sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
 echo "install kubectl"
 sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get -qq update
-sudo apt-get -y install kubelet, kubeadm, kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 echo "install jenkins on k8s"
 helm repo add jenkins https://charts.jenkins.io
