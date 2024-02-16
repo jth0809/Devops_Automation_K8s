9c78f361bf775ebbe482b56ff296b608117fb86e
jth0809 <jth0809@naver.com>
2024-02-16 21:10:53 +0900

    update file

--- install.sh
+++ install.sh
@@ -13,8 +13,22 @@ sudo apt-get -qq update
 sudo apt-get -y install helm
 
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
+
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
 sudo modprobe br_netfilter
-sudo sysctl -w net.ipv4.ip_forward=1
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
 
 echo ' '
 echo "install cri-o podman kubelet kubeadm kubectl"
